73960052258a44120076e1ebe8f091890efc8194
Faraz Ahmed <farazahmediu01@gmail.com>
2025-06-21 02:28:34 -0700

    Refactor

--- 01-Hello-Agent.py
+++ 01-Hello-Agent.py
@@ -8,23 +8,23 @@ import os
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
+
 external_client = AsyncOpenAI(
     api_key=api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client,
+)
+
+# global level configuration
 set_default_openai_client(external_client)
 set_tracing_disabled(True)
 
 
-agent = Agent(
-    name="Assistant",
-    instructions="You are a helpful assistant",
-    model=OpenAIChatCompletionsModel(
-        model="gemini-2.5-flash",
-        openai_client=external_client,
-    ),
-)
+agent = Agent(name="Assistant", instructions="You are a helpful assistant", model=model)
 
 
 result = Runner.run_sync(agent, "Hello How are you?")
